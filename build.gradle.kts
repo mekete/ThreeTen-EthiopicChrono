496dce76f99e8f8cc1e9a7100d6136d9b6d6de69
mekete <mail2teka@gmail.com>
2025-11-18 03:41:46 -0800

    Revert "Continue full Kotlin Multiplatform migration"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 buildscript {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
         classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     }
 }
 
